424831a61de835f609bff330438784c907b115a5
Sacralletius <Sacralletius@users.noreply.github.com>
2023-02-15 02:37:38 +0100

    Update plugin.cpp

--- plugin.cpp
+++ plugin.cpp
@@ -15,7 +15,10 @@ void SetupLog() {
     spdlog::flush_on(spdlog::level::trace);
 }
 
-////// code from Fenix for CastSpellFromHand() function.
+////// Instructions from Fenix to get the position and rotation for CastSpellFromHand() function.
+
+// ============ Step 1 ============
+// copied from FenixProjectilesAPI.h in https://github.com/fenix31415/MagicDeflectionAPI.
 struct ProjectileRot {
     float x, z;
 };
@@ -25,23 +28,58 @@ typedef uint32_t (*cast_t)(RE::Actor* caster, RE::SpellItem* spel, const RE::NiP
 typedef uint32_t (*cast_CustomPos_t)(RE::Actor* caster, RE::SpellItem* spel, const RE::NiPoint3& start_pos,
                                      const ProjectileRot& rot);
 
+// ============ Step 2 ============
+
+static cast_CustomPos_t _API_cast_CustomPos_t;
+
 uint32_t API_cast_CustomPos_t(RE::Actor* caster, RE::SpellItem* spel, const RE::NiPoint3& start_pos,
                               const ProjectileRot& rot) {
     return (*_API_cast_CustomPos_t)(caster, spel, start_pos, rot);
 }
 
-// I need to make another DLL for this?
-void init() {
-    _API_cast_CustomPos_t = (cast_CustomPos_t)GetProcAddress(pluginHandle, "FenixProjectilesAPI_cast_CustomPos");       // does this work with all versions of Skyrim? 1.5 and 1.6+? Otherwise some users of One-Handed Crossbow Framework might not like it.
+// ============ Step 3 ============
+// This apparently needs another DLL?
+// I'm not sure how this will work. AFAIK this references to typedefs in step 1.
+
+namespace Subscribers {
+    void init() {
+        _API_cast_CustomPos_t = (cast_CustomPos_t)GetProcAddress(pluginHandle, "FenixProjectilesAPI_cast_CustomPos");
+    }
 }
 
+// ============ Step 4 ============
 static void SKSEMessageHandler(SKSE::MessagingInterface::Message* message) {
     switch (message->type) {
         case SKSE::MessagingInterface::kDataLoaded:
-            _API_cast_CustomPos_t::init();
+            Subscribers::init();
+
             break;
     }
 }
+
+// ============ Step 7 ============
+float SkyrimSE_c51f70(RE::NiPoint3* dir) {
+    using func_t = decltype(SkyrimSE_c51f70);
+    REL::Relocation<func_t> func{REL::ID(68820)};
+    return func(dir);
+}
+
+auto rot_at(RE::NiPoint3 dir) {
+    ProjectileRot rot;
+    auto len = dir.Unitize();
+    if (len == 0) {
+        rot = {0, 0};
+    } else {
+        float polar_angle = SkyrimSE_c51f70(&dir);
+        rot = {-asin(dir.z), polar_angle};
+    }
+    return rot;
+}
+
+auto rot_at(const RE::NiPoint3& from, const RE::NiPoint3& to) { return rot_at(to - from); }
+
+// This line is needed for CastSpellFromHand() to compile, might no longer be needed in the future? ---> This is a dtor.
+// RE::Projectile::LaunchData::~LaunchData() {}
 
 ///// Actual new functions /////
 
@@ -72,75 +110,55 @@ void CastIngredient(RE::StaticFunctionTag*, RE::Actor* akSource, RE::IngredientI
 // Cast a spell from the hand defined in PositionInt at the akTarget.
 void CastSpellFromHand(RE::StaticFunctionTag*, RE::Actor* akSource, RE::SpellItem* akSpell, RE::TESObjectREFR* akTarget,
                        std::int32_t PositionInt, RE::BGSProjectile* akProjectile) {
-    RE::Projectile::LaunchData ldata;
-
+    
     auto NodePosition = akSource
                             ->GetMagicCaster(PositionInt == 0 ? RE::MagicSystem::CastingSource::kLeftHand
                                                               : RE::MagicSystem::CastingSource::kRightHand)
                             ->GetMagicNode()
                             ->world.translate;
 
-    ldata.origin = NodePosition;
-    ldata.shooter = akSource;
-    ldata.desiredTarget = akTarget;
-    ldata.spell = akSpell;
-    ldata.castingSource =
-        PositionInt == 0 ? RE::MagicSystem::CastingSource::kLeftHand : RE::MagicSystem::CastingSource::kRightHand;
+    auto rot = rot_at(NodePosition);
 
+    API_cast_CustomPos_t(akSource, akSpell, NodePosition, rot);
+
+//    old code, kept as backup.
+
+//    RE::Projectile::LaunchData ldata;
+
+//    ldata.origin = NodePosition;
+//    ldata.shooter = akSource;
+//    ldata.desiredTarget = akTarget;
+//    ldata.spell = akSpell;
+//    ldata.castingSource =
+//        PositionInt == 0 ? RE::MagicSystem::CastingSource::kLeftHand : RE::MagicSystem::CastingSource::kRightHand;
+//    ldata.projectileBase = akProjectile;
+// 
+// 
 //   remove all nullptr's.
 // 
 //   ldata.contactNormal = nullptr; // doesn't like nullptr
-     ldata.projectileBase = akProjectile;
 //    ldata.combatController = nullptr;
 //    ldata.weaponSource = nullptr;
 //    ldata.ammoSource = nullptr; //Might be useful to experiment with ammo...
 //    ldata.unk50 = nullptr;
-    ldata.unk60 = 0.f;
-    ldata.unk64 = 0.f;
+//    ldata.unk60 = 0.f;
+//    ldata.unk64 = 0.f;
 //    ldata.enchantItem = nullptr;
 //    ldata.poison = nullptr;
-    ldata.area = 0;
-    ldata.power = 1.f;
-    ldata.scale = 1.f;
-    ldata.alwaysHit = false;
-    ldata.noDamageOutsideCombat = false;
-    ldata.autoAim = false;
-    ldata.unk9F = false;
-    ldata.useOrigin = true;
-    ldata.deferInitialization = false;
-    ldata.forceConeOfFire = false;
+//    ldata.area = 0;
+//    ldata.power = 1.f;
+//    ldata.scale = 1.f;
+//    ldata.alwaysHit = false;
+//    ldata.noDamageOutsideCombat = false;
+//    ldata.autoAim = false;
+//    ldata.unk9F = false;
+//    ldata.useOrigin = true;
+//    ldata.deferInitialization = false;
+//    ldata.forceConeOfFire = false;
     
-    // not sure this exists.
-    ldata.rotation = rot_at;
-    
-    // this needs to go here.
-    float SkyrimSE_c51f70(RE::NiPoint3 * dir) {
-        using func_t = decltype(SkyrimSE_c51f70);
-        REL::Relocation<func_t> func{REL::ID(68820)};
-        return func(dir);
-    }
-
-    auto rot_at(RE::NiPoint3 dir) {
-        ProjectileRot rot;
-        auto len = dir.Unitize();
-        if (len == 0) {
-            rot = {0, 0};
-        } else {
-            float polar_angle = SkyrimSE_c51f70(&dir);
-            rot = {-asin(dir.z), polar_angle};
-        }
-        return rot;
-    }
-
-    auto rot_at(const RE::NiPoint3& from, const RE::NiPoint3& to) { return rot_at(to - from); }
-
-     RE::BSPointerHandle<RE::Projectile> handle;
-     RE::Projectile::Launch(&handle, ldata);
+//     RE::BSPointerHandle<RE::Projectile> handle;
+//     RE::Projectile::Launch(&handle, ldata);
 }
-
-// This line is needed for CastSpellFromHand() to compile, might no longer be needed in the future?
-// RE::Projectile::LaunchData::~LaunchData() {}
-
 
 bool PapyrusFunctions(RE::BSScript::IVirtualMachine* vm) {
     vm->RegisterFunction("CastEnchantment", "ANDR_PapyrusFunctions", CastEnchantment);
